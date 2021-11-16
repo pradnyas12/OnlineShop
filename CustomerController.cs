e5197a38cf347dc14b14ee688526f9763133fb22
pradnyas12 <59991173+pradnyas12@users.noreply.github.com>
2021-11-16 15:25:17 +0530

    Add files via upload

---
+++ CustomerController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BalLibrary;
+using DalLibrary;
+using System.Web.Http.Cors;
+
+
+namespace WebApplication1.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+
+    public class CustomerController : ApiController
+    {
+
+        public string Get()
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public IHttpActionResult Customer(CustomerBAL ba)
+        {
+            DALlayer ds = new DALlayer();
+            bool ans = ds.CustCheck(ba);
+            if (ans == false)
+            {
+                return Ok(new { status = 401, isSuccess = false, message = "Invalid user", });
+
+            }
+            else
+            {
+                return Ok(new { status = 200, isSuccess = true, message = "Valid user" });
+
+            }
+
+        }
+    }
+}
---
+++ DALlayer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BalLibrary;
+
+namespace DalLibrary
+{
+    public class DALlayer
+    {
+        public bool Check(CustomerBAL ba)
+        {
+            SqlConnection cn = new SqlConnection("Data Source=DESKTOP-K1I5LU7\\SQLEXPRESS;Integrated Security=true;database=OnlineShopping");
+            string s = "Select[dbo].[AdminLoginFunction](@p_aid,@p_pwd)";
+            SqlCommand cmd = new SqlCommand(s, cn);
+            cmd.Parameters.AddWithValue("@p_aid", ba.AdminId);
+            cmd.Parameters.AddWithValue("@p_pwd", ba.AdminPassword);
+
+            cn.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            rdr.Read();
+            bool ans=false;
+            if (Convert.ToInt32(rdr[0]) == 1)
+            {
+                //Console.WriteLine("u r welcome"); 
+                ans = true;
+            }
+            else
+            {
+                //Console.WriteLine("bye"); 
+                ans = false;
+            }
+            cn.Close();
+            cn.Dispose();
+            return ans;
+        }
+
+        public bool CustCheck(CustomerBAL ba)
+        {
+            SqlConnection cn = new SqlConnection("Data Source=DESKTOP-K1I5LU7\\SQLEXPRESS;Integrated Security=true;database=OnlineShopping");
+            string s = "Select[dbo].[CustomerLoginFunction](@u_uid,@u_pswd)";
+            SqlCommand cmd = new SqlCommand(s, cn);
+            cmd.Parameters.AddWithValue("@u_uid", ba.EmailId);
+            cmd.Parameters.AddWithValue("@u_pswd", ba.Password);
+
+            cn.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            rdr.Read();
+            bool ans = true;
+            if (Convert.ToInt32(rdr[0]) == 1)
+            {
+                //Console.WriteLine("u r welcome"); 
+                ans = true;
+            }
+            else
+            {
+                //Console.WriteLine("bye"); 
+                ans = false;
+            }
+            cn.Close();
+            cn.Dispose();
+            return ans;
+        }
+    }
+}
